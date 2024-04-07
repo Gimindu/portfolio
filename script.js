3139c92f18a7eb0c2eab47f61e5cda64f1253471
gimindugenuka <gimindu.20220282@iit.ac.lk>
2024-04-07 11:37:25 +0530

    Add scroll reveal library

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ submit.addEventListener('click',()=>{
     document.body.remove(link);
     window.location.href = "contact.html"
 })
+
+/* =============================== scroll reaveal ================================== */
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
